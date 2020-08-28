21129fe2a30ebb72fca60f470e0e7f52a9cd9de5
cfue <curtis.ch.chat@gmail.com>
2020-08-27 21:25:55 -0600

    connect server to mongoose and check production environment

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
@@ -9,6 +12,15 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.error("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 
