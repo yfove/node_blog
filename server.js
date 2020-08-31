6ad689d0ba575d4cc5048aceafb8f1489ad4a9c7
cfue <curtis.ch.chat@gmail.com>
2020-08-30 18:36:13 -0600

    create routes for authors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -17,11 +18,11 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
-
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.error("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
