a39c8ad2359470d031e1e4a0049fcf28720202ba
yfove <curtis.ch.chat@gmail.com>
2020-09-25 17:31:00 -0700

    add edit article function, bug in article routes

--- routes/articles.js
+++ routes/articles.js
@@ -7,6 +7,11 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
+});
+
 router.get("/:slug", async (req, res) => {
   console.log(req.params.id); // log the params id
   const article = await Article.findOne({ slug: req.params.slug });
@@ -16,20 +21,23 @@ router.get("/:slug", async (req, res) => {
 
 // this is going to be an async function so we need to set it up as so
 
-router.post("/", async (req, res) => {
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  });
-  try {
-    article = await article.save();
-    res.redirect(`/articles/${article.slug}`);
-  } catch (e) {
-    console.log(e);
-    res.render("articles/new", { article: article });
-  }
-});
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+router.put(
+  "/:id",
+  async (req, res) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
 
 // we meed use a library called method override to delete
 // in THML forms only GET and POST are allowed so we have to use middleware
@@ -38,5 +46,21 @@ router.delete("/:id", async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (e) {
+      console.log(e);
+      res.render("articles/#{path}", { article: article });
+    }
+  };
+}
 
 module.exports = router;
