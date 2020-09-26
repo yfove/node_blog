931d3604163ae67a8073d9db0b7bd33bddb15715
yfove <curtis.ch.chat@gmail.com>
2020-09-26 11:44:59 -0700

    fix for article edit bugs

--- routes/articles.js
+++ routes/articles.js
@@ -9,33 +9,28 @@ router.get("/new", (req, res) => {
 
 router.get("/edit/:id", async (req, res) => {
   const article = await Article.findById(req.params.id);
-  res.render("articles/edit", { article: article });
+  res.render('articles/edit', { article: article });
 });
 
 router.get("/:slug", async (req, res) => {
   console.log(req.params.id); // log the params id
   const article = await Article.findOne({ slug: req.params.slug });
   if (article == null) res.redirect("/");
-  res.render(`articles/show`, { article: article });
+  res.render('articles/show', { article: article });
 });
 
 // this is going to be an async function so we need to set it up as so
 
-router.post(
-  "/",
-  async (req, res, next) => {
-    req.article = new Article();
-    next();
-  },
-  saveArticleAndRedirect("new")
-);
+router.post("/", async (req, res, next) => {
+  req.article = new Article();
+  next();
+},
+  saveArticleAndRedirect("new"));
 
-router.put(
-  "/:id",
-  async (req, res) => {
-    req.article = await Article.findById(req.params.id);
-    next();
-  },
+router.put("/:id", async (req, res, next) => {
+  req.article = await Article.findById(req.params.id);
+  next();
+},
   saveArticleAndRedirect("edit")
 );
 
@@ -49,18 +44,18 @@ router.delete("/:id", async (req, res) => {
 
 function saveArticleAndRedirect(path) {
   return async (req, res) => {
-    let article = req.article;
-    article.title = req.body.title;
-    article.description = req.body.description;
-    article.markdown = req.body.markdown;
+    let article = req.article
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
     try {
       article = await article.save();
       res.redirect(`/articles/${article.slug}`);
     } catch (e) {
       console.log(e);
-      res.render("articles/#{path}", { article: article });
+      res.render(`articles/#{path}`, { article: article });
     }
-  };
+  }
 }
 
 module.exports = router;
